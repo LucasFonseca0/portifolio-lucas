b608bc73b1ab628d2baa7706ae23a3c16cd3d412
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-04-24 05:49:55 +0100

    feat: removed non used images, and added SVG images

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,12 +19,12 @@ export default function Home() {
   return (
     <div className="relative w-full ">
 
-      <div className="relative flex flex-col  h-[90vh] w-full bg-[url('/images/background9V.png')] md:bg-[url('/images/background9.png')]  bg-cover bg-center">
+      <div className="relative flex flex-col  h-[90vh] w-full bg-[url('/images/backgroundV.svg')] md:bg-[url('/images/background.svg')]  bg-cover bg-center">
 
       
         <header className=" animate-fade-in-up flex justify-between  h-52 pl-6  sm:pl-14 md:pl-20 z-10 relative">
           <div className="flex items-center gap-3 ">
-            <Avatar src="/images/photo.jpg" className=" w-16 h-16 sm:w-20 sm:h-20 md:w-28 md:h-28 text-large relative z-20" alt={t('name')} />
+            <Avatar showFallback src="/images/photo.svg" className=" w-16 h-16 sm:w-20 sm:h-20 md:w-28 md:h-28 text-large relative z-20" alt={t('name')} />
             <div className="relative z-20 text:xl sm:text-2xl md:text-4xl">
               <h1 className="font-medium">{t('name')}</h1>
               <h2>{t('role')}</h2>
