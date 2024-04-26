cf4f153f9a8be8cf9b534d5ab66dc53e32aceec0
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-04-26 07:09:53 +0100

    change style: styled skills divs

--- app/animation/TranslateLeft.animation.tsx
+++ app/animation/TranslateLeft.animation.tsx
@@ -12,7 +12,7 @@ export default function TranslateLeft({ children }:any) {
         style={{
           transform: isInView ? "none" : "translateX(-100px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+          transition: "all 0.4s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
         }}
         className="block  "
       >
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import TranslateLeft from "../animation/TranslateLeft.animation";
 
 import SkillsSVG from "../SVGs/SkillsSVG";
+import TranslateRight from "../animation/TranslateRight";
 
 const Skills = () => {
   return (
@@ -16,9 +17,47 @@ const Skills = () => {
         </span>
         SKILLS
       </h2>
-      <div>
-        <SkillsSVG logoName="typescript" />
-        
+      <div className=" w-full flex flex-col [&>div]:flex [&>div]:gap-1 [&>div]:items-center [&>div]:w-full  [&>*:nth-child(odd)]:flex-row-reverse">
+        <div>
+          <TranslateRight>
+            <SkillsSVG logoName="typescript" className="w-10 h-full" />
+          </TranslateRight>
+          <TranslateRight>
+            <p>Typescript</p>
+          </TranslateRight>
+        </div>
+        <div>
+          <TranslateLeft>
+            <SkillsSVG logoName="next" className="w-10 h-full" />
+          </TranslateLeft>
+          <TranslateLeft>
+            <p>NextJS</p>
+          </TranslateLeft>
+        </div>
+        <div>
+          <TranslateRight>
+            <SkillsSVG logoName="tailwind" className="w-10 h-full" />
+          </TranslateRight>{" "}
+          <TranslateRight>
+            <p>Tailwind CSS</p>
+          </TranslateRight>
+        </div>
+        <div>
+          <TranslateLeft>
+            <SkillsSVG logoName="nest" className="w-10 h-full" />
+          </TranslateLeft>
+          <TranslateLeft>
+            <p>NestJS</p>
+          </TranslateLeft>
+        </div>
+        <div>
+          <TranslateRight>
+            <SkillsSVG logoName="database" className="w-10 h-full" />
+          </TranslateRight>{" "}
+          <TranslateRight>
+            <p>Databases SQL and noSQL</p>
+          </TranslateRight>
+        </div>
       </div>
     </section>
   );
