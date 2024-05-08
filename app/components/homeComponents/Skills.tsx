605ba5c66d8ce01d4f7f386f17699581317e5cd3
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-05-08 06:21:05 +0100

    feat: skills field started

--- app/components/homeComponents/Skills.tsx
+++ app/components/homeComponents/Skills.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { BsArrowLeft } from 'react-icons/bs'
+import { IoIosArrowRoundBack } from 'react-icons/io'
 
 const Skills = () => {
   return (
-    <div className='h-[100vh]'>
-      
+    <div className='h-[100vh] text-black '>
+      <div className='flex items-center'>
+        <h2 className='text-7xl font-bold'>Skills</h2>
+        <BsArrowLeft className='text-primaryDarkest h20 w-40  ' height={100} width={100} size={400} />
+      </div>
     </div>
   )
 }
