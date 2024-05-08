8149460c39b5531498312c0f5f2f760747a08d41
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-05-08 05:03:20 +0100

    feat: about me photo and text added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Introduction from "../components/homeComponents/Introduction";
+import AboutMe from "../components/homeComponents/AboutMe";
 import Skills from "../components/homeComponents/Skills";
-
 
 export default function Home() {
   return (
     <div className="relative bg-white w-full ">
       <Introduction />
-     
+
+      <AboutMe />
       <Skills />
     </div>
   );
--- app/components/homeComponents/Skills.tsx
+++ app/components/homeComponents/Skills.tsx
@@ -1,17 +1,11 @@
-import TranslateAnimation from "@/app/animation/TranslateAnimation.animation";
-import SkillsSVG from "@/app/SVGs/SkillsSVG";
+import React from 'react'
 
 const Skills = () => {
-
-    const logoNames:LogoName[]=["typescript","docker","git","mongodb","nest","next","react","tailwind","database"]
-
   return (
-    <section className="h-[100vh]">
-      <div className="w-full h-[11vh] relative bg-primary rounded-ee-[100px]">
-        <div className="absolute top-[100%] -translate-y-1/2 h-32 w-96 rounded-b-[60px] rounded-t-[20px] bg-black flex justify-center items-center text-primaryDarkest font-bold text-6xl">About me</div>
-      </div>
-    </section>
-  );
-};
+    <div className='h-[100vh]'>
+      
+    </div>
+  )
+}
 
-export default Skills;
+export default Skills
