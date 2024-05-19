51adb9d8d1e0951dede50633e719444729e59266
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-05-19 01:19:13 +0100

    build: vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {Poppins } from "next/font/google";
 
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import { NextUIProvider } from "@nextui-org/react";
 
 const poppins = Poppins({
